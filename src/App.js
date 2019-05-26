d1354cde3a5fe2d5c51b3b682a7b27e06ec5563d
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-25 21:30:17 -0500

    Writing components using React.createElement() -- serves as comparison against JSX component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello from React App!</h1>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h1>Hello from React App!</h1>
+    //   </div>
+    // );
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "Hello from React App!")
     );
   }
 }
