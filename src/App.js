4ea201663381b548efd2f8c6f453d8e780f152ce
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-25 21:40:10 -0500

    Add new functional component -- Person.js; add import in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./components/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello from React App!</h1>
+        <Person />
       </div>
     );
     // return React.createElement(
---
+++ src/components/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Person = () => {
+  return <p>I am a person and my name is Anakin</p>;
+};
+
+export default Person;
