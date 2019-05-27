acd0d8a48c6a42fa258c84a0dc4f99a333d53b46
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-27 14:48:17 -0500

    Refactor use of styling defined in Person.css file to in-line/scoped style in styled-components fashion. Add media query selector on styled component in Person.js

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,15 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import "./Person.css";
 
 const Person = props => {
+  const PersonDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    @media (min-width: 500px) {
+      width: 450px;
+    }
+  `;
+
   return (
-    <div className="Person">
+    <PersonDiv>
       <p onClick={props.click}>
         I am a person and my name is {props.name}. I am {props.age} years old
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    </PersonDiv>
   );
 };
 
