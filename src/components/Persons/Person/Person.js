8b98758c358493907041e025ad14405824e80abe
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-27 23:08:46 -0500

    Convert Person.js from functional to class component to leverage component lifecycle methods

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import "./Person.css";
 
-const Person = props => {
-  const PersonDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-    @media (min-width: 500px) {
-      width: 450px;
-    }
-  `;
+class Person extends Component {
+  render() {
+    return (
+      <PersonDiv>
+        <p onClick={this.props.click}>
+          I am a person and my name is {this.props.name}. I am {this.props.age}{" "}
+          years old
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </PersonDiv>
+    );
+  }
+}
 
-  return (
-    <PersonDiv>
-      <p onClick={props.click}>
-        I am a person and my name is {props.name}. I am {props.age} years old
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </PersonDiv>
-  );
-};
+const PersonDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
 
 export default Person;
